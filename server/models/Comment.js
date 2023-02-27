f23b3a51b75d987aff2f5f8262a2c6f96eab7809
nadasuhailAyesh12 <missnadaayesh@gmail.com>
2023-02-28 00:59:18 +0200

    create comment model

---
+++ server/models/Comment.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const commentSchema = new mongoose.Schema({
+    post: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+        required: [true, "Post is required"]
+    },
+    user: {
+        type: Object,
+        required: [true, "User is required"]
+    },
+    description: {
+        type: string,
+        required: [true, "Description is required"]
+    }
+}, { timestamps: true })
+
+const Comment = mongoose.model('Comment', commentSchema)
+module.exports = Comment
